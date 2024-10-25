3ade2cc65c4b7144e11daf5cd9507645ffc8c7a7
luka mkalavishvili <mkalavishvililuka00@gmail.com>
2024-10-25 15:50:12 +0400

    layout / header update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,20 +2,12 @@ import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
 
 function Layout() {
-    return (
-        <div>
-            <div className="">
-
-            <Header />
-            </div>
-
-            <div className="mt-40">
-            <Outlet />
-            </div>
-
-
-        </div>
-    )
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  )
 }
 
 export default Layout
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,49 +8,49 @@ import { useState } from "react";
 
 
 function Header() {
-    let [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const {pathname} = useResolvedPath()
-        return (
-        <div className="flex justify-center bg-black">
+  let [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const { pathname } = useResolvedPath()
+  return (
+    <header className="flex justify-center bg-black fixed top-0 z-50  w-full">
 
-        <div className="flex justify-between items-center w-[80vw] mx-auto py-10  border-white fixed top-0 z-50 ">
-            <Link to='/'>logo</Link>
-            <div className="hidden md:flex min-w-[25vw] justify-around border-gray-700 border-2 py-5 px-2 rounded-2xl bg-zinc-950">
-                <Link to='/' className={`p-3 rounded-xl ${pathname === '/' && 'bg-zinc-800'}`}>მთავარი</Link>
-                <Link to='/movies' className={`p-3 rounded-xl ${pathname === '/movies' && 'bg-zinc-800'}`}>ფილმები</Link>
-                <Link to='/serials' className={`p-3 rounded-xl ${pathname === '/serials' && 'bg-zinc-800'}`}>სერიალები</Link>
-                <Link to='animes' className={`p-3 rounded-xl ${pathname === '/animes' && 'bg-zinc-800'}`}>ანიმეები</Link>
-            </div>
-            <div className="hidden md:flex gap-5">
-                <div className="text-2xl"><HiMagnifyingGlass /></div>
-                <Link to='login' className="text-2xl"><HiOutlineUserCircle /></Link>
-            </div>
+      <div className="flex justify-between items-center w-[80%] mx-auto py-5 border-white ">
+        <Link to='/'>logo</Link>
+        <div className="hidden md:flex min-w-[25vw] justify-around border-gray-700 border-2 py-5 px-2 rounded-2xl bg-zinc-950">
+          <Link to='/' className={`p-3 rounded-xl ${pathname === '/' && 'bg-zinc-800'}`}>მთავარი</Link>
+          <Link to='/movies' className={`p-3 rounded-xl ${pathname === '/movies' && 'bg-zinc-800'}`}>ფილმები</Link>
+          <Link to='/serials' className={`p-3 rounded-xl ${pathname === '/serials' && 'bg-zinc-800'}`}>სერიალები</Link>
+          <Link to='animes' className={`p-3 rounded-xl ${pathname === '/animes' && 'bg-zinc-800'}`}>ანიმეები</Link>
+        </div>
+        <div className="hidden md:flex gap-5">
+          <div className="text-2xl"><HiMagnifyingGlass /></div>
+          <Link to='login' className="text-2xl"><HiOutlineUserCircle /></Link>
+        </div>
 
-            <div className="md:hidden text-3xl">
-                <div onClick={() => setIsSidebarOpen(is => !is)}>    
-                    <HiBars3 />  
+        <div className="md:hidden text-3xl">
+          <div onClick={() => setIsSidebarOpen(is => !is)}>
+            <HiBars3 />
+          </div>
+          {isSidebarOpen && (
+            <div className="absolute left-0 top-15 w-[100vw] h-[100vh] z-50">
+              <div className="flex flex-col  gap-5 mt-10 ml-5">
+                <div className="flex flex-col gap-5">
+                  <Link to='/' onClick={() => setIsSidebarOpen(false)}>მთავარი</Link>
+                  <Link to='/movies' onClick={() => setIsSidebarOpen(false)}>ფილმები</Link>
+                  <Link to='/serials' onClick={() => setIsSidebarOpen(false)}>სერიალები</Link>
+                  <Link to='animes' onClick={() => setIsSidebarOpen(false)}>ანიმაციები</Link>
                 </div>
-                {isSidebarOpen && (
-                    <div className="absolute left-0 top-15 w-[100vw] h-[100vh] z-50">
-                    <div className="flex flex-col  gap-5 mt-10 ml-5">
-                       <div className="flex flex-col gap-5">
-                           <Link to='/' onClick={() =>  setIsSidebarOpen(false)}>მთავარი</Link>
-                           <Link to='/movies' onClick={() => setIsSidebarOpen(false)}>ფილმები</Link>
-                           <Link to='/serials' onClick={() => setIsSidebarOpen(false)}>სერიალები</Link>
-                           <Link to='animes' onClick={() => setIsSidebarOpen(false)}>ანიმაციები</Link>
-                       </div>
-                       <div className="flex flex-col gap-5 mt-10">
-                           <div>search</div>
-                           <Link to='/login' onClick={() => setIsSidebarOpen(false)}>პროფილი</Link>
-                       </div>
-                    </div>
-               </div>
-                )}
+                <div className="flex flex-col gap-5 mt-10">
+                  <div>search</div>
+                  <Link to='/login' onClick={() => setIsSidebarOpen(false)}>პროფილი</Link>
+                </div>
+              </div>
             </div>
+          )}
         </div>
-        </div>
+      </div>
+    </header>
 
-    )
+  )
 }
 
 export default Header

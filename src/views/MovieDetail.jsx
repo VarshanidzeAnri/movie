4011c907c994158d4bc18f68cb40e68d2bced991
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-10-26 00:59:22 +0400

    movie details page

--- src/views/MovieDetail.jsx
+++ src/views/MovieDetail.jsx
@@ -1,7 +1,26 @@
 function MovieDetail() {
     return (
-        <div>
-            test
+        <div className="w-[70vw] mx-auto">
+            <div className="bg-zinc-800 p-5">
+                
+                <div className="flex justify-start gap-5">
+                    <div className="w-[50%] h-full"><img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" className="w-full h-full" /></div>
+                    <div className="w-[50%] flex flex-col gap-3">
+                        <div>
+                            <div className="text-3xl mt-2">ლურჯი საკეტი</div>
+                            <div className="text-2xl mt-2">Bluelock</div>
+                        </div>
+                        <div>წელი: 2024</div>
+                        <div>ჟანრი: დრამა, კომედია, სათავგადასავლო</div>
+                        <div>რეჟისორი: ანრი ვარშანიძე, რეზი ბოლქვაძე</div>
+                        <div>მსახიობები: ანრი ვარშანიძე, რეზი ბოლქვაძე</div>
+                        <div className="w-full text-wrap text-justify mt-5 text-xl">აღწერა: <br /> იაპონიის საფეხბურთო ნაკრები მსოფლიო ჩემპიონატზე დამარცხდა, რაც ამ სპორტული მოედნის სახელმწიფო დონეზე ანალიზის საფუძველი გახდა. განცდილი ზარალი, გარკვეულწილად, ქვეყნის რეპუტაციის დარტყმა იყო. მოქმედება იყო საჭირო. ამიტომაც წამოიწყო პროექტი Blue Lock. ამ პროექტის ხელმძღვანელობა ჯინპაჩი ეგოს დაევალა. ამ ადამიანზეა დამოკიდებული, შეძლებს თუ არა საფეხბურთო ნაკრები მომავალ ჩემპიონატზე გამარჯვებას. პროექტის ფარგლებში აუცილებელია იდეალური თავდამსხმელის შექმნა და დამუშავება, რათა ის იყოს ნამდვილი პროფესიონალი და წარმოუდგენლად დარწმუნებული თავის შესაძლებლობებში.</div>
+                    </div>
+                </div>
+                <div>
+                
+                </div>
+            </div>
         </div>
     )
 }

c1a044cf19fe2dcdb1f05cd1792fa91476bba367
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-10-25 16:52:08 +0400

    changing header carousel

--- src/components/HeaderCarousel.jsx
+++ src/components/HeaderCarousel.jsx
@@ -1,42 +1,38 @@
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import { MOVIES } from '../../data';
-
-
-
+import 'swiper/css/autoplay';
+import './changeSwiper.css'
 
 function HeaderCarousel() {
     return (
-      <div className='w-full md:w-[70vw] mx-auto'>
-      <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      autoplay
-      time
-      onSwiper={(swiper) => console.log(swiper)}
-      loop
-    >
-      {MOVIES.map(movie => (
+      <div className='w-full md:w-[70vw] mx-auto '>
+        <Swiper
+        modules={[Navigation, Pagination, A11y, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        speed={1000}
+        autoplay={{
+          delay: 3000,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false,
+        }}  
+        loop={true}
+      >
+        {MOVIES.map(movie => (
+        <SwiperSlide key={movie.id}>
+          <img src={movie.image} className='w-full h-[30vh] md:h-[70vh] object-cover' />
+        </SwiperSlide>
+        ))}
 
-      
-      <SwiperSlide key={movie.id}>
-        <img src={movie.image} className='w-full h-[30vh] md:h-[70vh] object-cover' />
-      </SwiperSlide>
-      ))}
-
-    </Swiper>
+      </Swiper>
     </div>
     )
 }

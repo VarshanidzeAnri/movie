8764a5e5225c869432f292ef8d7380fc01b6e68e
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-10-27 00:13:20 +0400

    changing movie detauls

--- src/views/MovieDetail.jsx
+++ src/views/MovieDetail.jsx
@@ -10,7 +10,7 @@ function MovieDetail() {
                     <div className="h-[30vh] md:h-[50vh] w-full lg:w-[70%]"><img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" className="w-full h-full object-cover rounded-md" /></div>
                 </div>
 
-                <div className="flex flex-col gap-2 md:gap-5 p-5 pt-2 bg-zinc-800 w-full rounded-md">
+                <div className="flex flex-col gap-2 md:gap-3 p-5 pt-2 bg-zinc-800 w-full rounded-md">
                         <div>
                             <div className="text-2xl mt-2">ლურჯი საკეტი</div>
                             <div className="text-xl mt-2">Bluelock</div>
@@ -28,7 +28,7 @@ function MovieDetail() {
                             </div>
                         </div>
                         
-                        <div className="w-full text-wrap text-sm flex flex-col gap-2 mt-2">
+                        <div className="w-full text-wrap text-sm flex flex-col gap-2 ">
                             <div>მოკლე აღწერა:</div>
                             <div>იაპონიის საფეხბურთო ნაკრები მსოფლიო ჩემპიონატზე დამარცხდა, რაც ამ სპორტული მოედნის სახელმწიფო დონეზე ანალიზის საფუძველი გახდა. განცდილი ზარალი, გარკვეულწილად, ქვეყნის რეპუტაციის დარტყმა იყო. მოქმედება იყო საჭირო. ამიტომაც წამოიწყო პროექტი Blue Lock. ამ პროექტის ხელმძღვანელობა ჯინპაჩი ეგოს დაევალა. ამ ადამიანზეა დამოკიდებული, შეძლებს თუ არა საფეხბურთო ნაკრები მომავალ ჩემპიონატზე გამარჯვებას. პროექტის ფარგლებში აუცილებელია იდეალური თავდამსხმელის შექმნა და დამუშავება, რათა ის იყოს ნამდვილი პროფესიონალი და წარმოუდგენლად დარწმუნებული თავის შესაძლებლობებში.</div>
                         </div>
